f703f6667f898c7ecedde8cd8d59c193c58d500d
201635848 <law9560@gmail.com>
2020-08-01 20:55:52 +0900

    Player Script 생서

---
+++ RPG/Assets/Script/Player/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public int Turn;
+
+    public PlayerControl instance;
+
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        instance = this;
+        Turn = 0;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void PlayerMove()
+    {
+
+    }
+}
