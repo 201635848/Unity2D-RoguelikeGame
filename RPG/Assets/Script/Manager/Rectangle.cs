6a005d061f3ac522e5ff38c359c3a7e6bdee487f
201635848 <law9560@gmail.com>
2020-09-04 21:38:11 +0900

    맵 생성 BSP 방식 진행 중

---
+++ RPG/Assets/Script/Manager/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rectangle : MonoBehaviour
+{
+    private static int MIN_SIZE = 5;
+    private static Random rnd = new Random();
+
+    private int top, left, width, height;
+    private Rectangle leftChild;
+    private Rectangle rightChild;
+    private Rectangle dungeon;
+
+    public Rectangle(int top, int left, int height, int width)
+    {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public bool split()
+    {
+        if (leftChild != null) //이미 분할되었다면,  할 필요 X
+            return false;
+        bool horizontal = rnd.nextBoolean();
+
+    }
+}
---
+++ RPG/Assets/Script/Manager/TileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    int roomWidth = 50;
+    int roomHeight = 50;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    public void createMap(int floor)
+    {
+
+        //Retangle의 split 호출
+    }
+
+
+
+}
