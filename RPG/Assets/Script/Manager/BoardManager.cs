8a2ebccfad4ae75ac1611df907effe16e4d600d9
201635848 <law9560@gmail.com>
2020-10-03 22:19:09 +0900

    BSP 적용 취소, Cellular Automata 구현

--- RPG/Assets/Script/Manager/BoardManager.cs
+++ RPG/Assets/Script/Manager/BoardManager.cs
@@ -1,313 +1,155 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-	public int boardRows, boardColumns;
-	public int minRoomSize, maxRoomSize;
-	public GameObject floorTile;
-	public GameObject corridorTile;
+    public int width;
+    public int height;
 
-	private GameObject[,] boardPositionsFloor;
-
-	public class SubDungeon
-	{
-		public SubDungeon left, right;
-		public Rect rect;
-		public Rect room = new Rect(-1, -1, 0, 0); // i.e null
-		public int debugId;
-		public List<Rect> corridors = new List<Rect>();
-
-		private static int debugCounter = 0;
-
-		public SubDungeon(Rect mrect)
-		{
-			rect = mrect;
-			debugId = debugCounter;
-			debugCounter++;
-		}
-
-		public bool IAmLeaf()
-		{
-			return left == null && right == null;
-		}
-
-		public bool Split(int minRoomSize, int maxRoomSize)
-		{
-			if (!IAmLeaf())
-			{
-				return false;
-			}
-
-			// choose a vertical or horizontal split depending on the proportions
-			// i.e. if too wide split vertically, or too long horizontally, 
-			// or if nearly square choose vertical or horizontal at random
-			bool splitH;
-			if (rect.width / rect.height >= 1.25)
-			{
-				splitH = false;
-			}
-			else if (rect.height / rect.width >= 1.25)
-			{
-				splitH = true;
-			}
-			else
-			{
-				splitH = Random.Range(0.0f, 1.0f) > 0.5;
-			}
-
-			if (Mathf.Min(rect.height, rect.width) / 2 < minRoomSize)
-			{
-				Debug.Log("Sub-dungeon " + debugId + " will be a leaf");
-				return false;
-			}
-
-			if (splitH)
-			{
-				// split so that the resulting sub-dungeons widths are not too small
-				// (since we are splitting horizontally) 
-				int split = Random.Range(minRoomSize, (int)(rect.width - minRoomSize));
-
-				left = new SubDungeon(new Rect(rect.x, rect.y, rect.width, split));
-				right = new SubDungeon(
-					new Rect(rect.x, rect.y + split, rect.width, rect.height - split));
-			}
-			else
-			{
-				int split = Random.Range(minRoomSize, (int)(rect.height - minRoomSize));
-
-				left = new SubDungeon(new Rect(rect.x, rect.y, split, rect.height));
-				right = new SubDungeon(
-					new Rect(rect.x + split, rect.y, rect.width - split, rect.height));
-			}
-
-			return true;
-		}
-
-		public void CreateRoom()
-		{
-			if (left != null)
-			{
-				left.CreateRoom();
-			}
-			if (right != null)
-			{
-				right.CreateRoom();
-			}
-			if (left != null && right != null)
-			{
-				CreateCorridorBetween(left, right);
-			}
-			if (IAmLeaf())
-			{
-				int roomWidth = (int)Random.Range(rect.width / 2, rect.width - 2);
-				int roomHeight = (int)Random.Range(rect.height / 2, rect.height - 2);
-				int roomX = (int)Random.Range(1, rect.width - roomWidth - 1);
-				int roomY = (int)Random.Range(1, rect.height - roomHeight - 1);
-
-				// room position will be absolute in the board, not relative to the sub-dungeon
-				room = new Rect(rect.x + roomX, rect.y + roomY, roomWidth, roomHeight);
-				Debug.Log("Created room " + room + " in sub-dungeon " + debugId + " " + rect);
-			}
-		}
+    public GameObject floorTile;
+    public GameObject wallTile;
 
 
-		public void CreateCorridorBetween(SubDungeon left, SubDungeon right)
-		{
-			Rect lroom = left.GetRoom();
-			Rect rroom = right.GetRoom();
+    public string seed;
+    public bool useRandomSeed;
 
-			Debug.Log("Creating corridor(s) between " + left.debugId + "(" + lroom + ") and " + right.debugId + " (" + rroom + ")");
+    [Range(0, 100)]
+    public int randomFillPercent;
 
-			// attach the corridor to a random point in each room
-			Vector2 lpoint = new Vector2((int)Random.Range(lroom.x + 1, lroom.xMax - 1), (int)Random.Range(lroom.y + 1, lroom.yMax - 1));
-			Vector2 rpoint = new Vector2((int)Random.Range(rroom.x + 1, rroom.xMax - 1), (int)Random.Range(rroom.y + 1, rroom.yMax - 1));
+    int[,] map;
+    private GameObject[,] boardPositionsFloor;
 
-			// always be sure that left point is on the left to simplify the code
-			if (lpoint.x > rpoint.x)
-			{
-				Vector2 temp = lpoint;
-				lpoint = rpoint;
-				rpoint = temp;
-			}
+    private void Start()
+    {
+        boardPositionsFloor = new GameObject[width, height];
+        GenerateMap();
+    }
 
-			int w = (int)(lpoint.x - rpoint.x);
-			int h = (int)(lpoint.y - rpoint.y);
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
 
-			Debug.Log("lpoint: " + lpoint + ", rpoint: " + rpoint + ", w: " + w + ", h: " + h);
+    private void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
 
-			// if the points are not aligned horizontally
-			if (w != 0)
-			{
-				// choose at random to go horizontal then vertical or the opposite
-				if (Random.Range(0, 1) > 2)
-				{
-					// add a corridor to the right
-					corridors.Add(new Rect(lpoint.x, lpoint.y, Mathf.Abs(w) + 1, 1));
+        for (int i = 0; i < 5; i++)
+        {
+            SmoothMap();
+        }
+        DrawMap();
 
-					// if left point is below right point go up
-					// otherwise go down
-					if (h < 0)
-					{
-						corridors.Add(new Rect(rpoint.x, lpoint.y, 1, Mathf.Abs(h)));
-					}
-					else
-					{
-						corridors.Add(new Rect(rpoint.x, lpoint.y, 1, -Mathf.Abs(h)));
-					}
-				}
-				else
-				{
-					// go up or down
-					if (h < 0)
-					{
-						corridors.Add(new Rect(lpoint.x, lpoint.y, 1, Mathf.Abs(h)));
-					}
-					else
-					{
-						corridors.Add(new Rect(lpoint.x, rpoint.y, 1, Mathf.Abs(h)));
-					}
+    }
 
-					// then go right
-					corridors.Add(new Rect(lpoint.x, rpoint.y, Mathf.Abs(w) + 1, 1));
-				}
-			}
-			else
-			{
-				// if the points are aligned horizontally
-				// go up or down depending on the positions
-				if (h < 0)
-				{
-					corridors.Add(new Rect((int)lpoint.x, (int)lpoint.y, 1, Mathf.Abs(h)));
-				}
-				else
-				{
-					corridors.Add(new Rect((int)rpoint.x, (int)rpoint.y, 1, Mathf.Abs(h)));
-				}
-			}
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
 
-			Debug.Log("Corridors: ");
-			foreach (Rect corridor in corridors)
-			{
-				Debug.Log("corridor: " + corridor);
-			}
-		}
+        //GetHashCode()를 하면 string 타입의 seed 에서 숫자를 뽑아내는데 렌덤한 숫자라 prng 로서 가치가 있는듯, 자세하게는 더 알아봐야할듯.
+        //prng는 pseudoRandomNumberGenerator 의 약자인데, 컴퓨터는 난수를 생성하지 못해서 일반적으로 난수를 위해서 seed가 필요하다.
+        //이때 이 난수를 위한 seed를 랜덤하게 생성해주는게 필요한데, 일반적으로 prng 라고 부르는듯하다.
+        System.Random prng = new System.Random(seed.GetHashCode());
 
-		public Rect GetRoom()
-		{
-			if (IAmLeaf())
-			{
-				return room;
-			}
-			if (left != null)
-			{
-				Rect lroom = left.GetRoom();
-				if (lroom.x != -1)
-				{
-					return lroom;
-				}
-			}
-			if (right != null)
-			{
-				Rect rroom = right.GetRoom();
-				if (rroom.x != -1)
-				{
-					return rroom;
-				}
-			}
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //아래 조건문은 그림을 그려보던가 해서 이해를 해야겠다... 아마도 정가운데 3*3공간에 1값을 넣어주는 듯 하다.
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    //랜덤퍼센트보다 낮으면 1을 반환하는데, 이는 1벽을 의미하고 0은 빈 공간을 의미한다.
+                    map[x, y] = (prng.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
 
-			// workaround non nullable structs
-			return new Rect(-1, -1, 0, 0);
-		}
-	}
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiels = GetSurroundingWallCount(x, y);
+                //이웃한 타일이 4개 초과면 자신도 벽이된다..
+                if (neighbourWallTiels > 4)
+                {
+                    map[x, y] = 1;
+                }
+                //이웃한 타일이 4개 미만이면 뚫린 공간이된다..
+                //즉 이웃한 타일이 많은쪽은 점점 많은쪽으로 벽이되고, 없는쪽은 점점 없는쪽으로 빈공간이 되는것.
+                else if (neighbourWallTiels < 4)
+                {
+                    map[x, y] = 0;
+                }
+            }
+        }
+    }
 
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        //아래 2중 for문은 gridX의 주면 3*3에 있는 이웃의 값을 가져온다.
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                //아래 이프문은 예방문인듯. 가로세로값 이내에서 이웃값을 계산하기위한 조건문
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                {
+                    //이웃의 값만 가져오기 위해 자기자신의 값을 제외한다
+                    //자기 자신을 빼고 이웃한 8개의 타일에서 벽의 개수를 카운트합니다
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    //이웃값을 계산할때 width, height값을 포함하지 않았으므로 테두리는 모두 벽으로 인정되며 카운트된다;
+                    wallCount++;
+                }
+            }
+        }
+        return wallCount;
+    }
 
-
-	public void CreateBSP(SubDungeon subDungeon)
-	{
-		Debug.Log("Splitting sub-dungeon " + subDungeon.debugId + ": " + subDungeon.rect);
-		if (subDungeon.IAmLeaf())
-		{
-			// if the sub-dungeon is too large split it
-			if (subDungeon.rect.width > maxRoomSize
-				|| subDungeon.rect.height > maxRoomSize
-				|| Random.Range(0.0f, 1.0f) > 0.25)
-			{
-
-				if (subDungeon.Split(minRoomSize, maxRoomSize))
-				{
-					Debug.Log("Splitted sub-dungeon " + subDungeon.debugId + " in "
-						+ subDungeon.left.debugId + ": " + subDungeon.left.rect + ", "
-						+ subDungeon.right.debugId + ": " + subDungeon.right.rect);
-
-					CreateBSP(subDungeon.left);
-					CreateBSP(subDungeon.right);
-				}
-			}
-		}
-	}
-
-	public void DrawRooms(SubDungeon subDungeon)
-	{
-		if (subDungeon == null)
-		{
-			return;
-		}
-		if (subDungeon.IAmLeaf())
-		{
-			for (int i = (int)subDungeon.room.x; i < subDungeon.room.xMax; i++)
-			{
-				for (int j = (int)subDungeon.room.y; j < subDungeon.room.yMax; j++)
-				{
-					GameObject instance = Instantiate(floorTile, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
-					instance.transform.SetParent(transform);
-					boardPositionsFloor[i, j] = instance;
-				}
-			}
-		}
-		else
-		{
-			DrawRooms(subDungeon.left);
-			DrawRooms(subDungeon.right);
-		}
-	}
-
-	void DrawCorridors(SubDungeon subDungeon)
-	{
-		if (subDungeon == null)
-		{
-			return;
-		}
-
-		DrawCorridors(subDungeon.left);
-		DrawCorridors(subDungeon.right);
-
-		foreach (Rect corridor in subDungeon.corridors)
-		{
-			for (int i = (int)corridor.x; i < corridor.xMax; i++)
-			{
-				for (int j = (int)corridor.y; j < corridor.yMax; j++)
-				{
-					if (boardPositionsFloor[i, j] == null)
-					{
-						GameObject instance = Instantiate(corridorTile, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
-						instance.transform.SetParent(transform);
-						boardPositionsFloor[i, j] = instance;
-					}
-				}
-			}
-		}
-	}
-
-	void Start()
-	{
-		SubDungeon rootSubDungeon = new SubDungeon(new Rect(0, 0, boardRows, boardColumns));
-		CreateBSP(rootSubDungeon);
-		rootSubDungeon.CreateRoom();
-
-		boardPositionsFloor = new GameObject[boardRows, boardColumns];
-		DrawRooms(rootSubDungeon);
-		DrawCorridors(rootSubDungeon);
-	}
+    private void DrawMap()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (map[x, y] == 1)
+                    {
+                        GameObject instance = Instantiate(wallTile, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                        instance.transform.SetParent(transform);
+                        boardPositionsFloor[x, y] = instance;
+                    }
+                    else if (map[x, y] == 0)
+                    {
+                        GameObject instance = Instantiate(floorTile, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                        instance.transform.SetParent(transform);
+                        boardPositionsFloor[x, y] = instance;
+                    }
+                }
+            }
+        }
+    }
 }
